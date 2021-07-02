6fff4c66e307da1dc7a56397b49e594047a0d433
Charles Tapsell <charlestapsell@Charless-MBP.lan>
2021-07-02 10:54:24 +0100

    f

--- clean_move.py
+++ clean_move.py
@@ -4,17 +4,9 @@ import shutil,os,glob,sys
 
 
 #create obj folder within darknet/data
-train_dir = 'darknet/data/obj'
+train_dir = 'obj'
 shutil.rmtree(train_dir, ignore_errors=True)
 os.makedirs(train_dir)
-
-
-
-#create test folder within darknet/data
-test_dir = 'darknet/data/test'
-shutil.rmtree(test_dir, ignore_errors=True)
-os.makedirs(test_dir)
-
 
 
 #cycle through each aug folder and move to data/obj
@@ -31,15 +23,6 @@ for source_dir in folders:
 
 
 
-#cycle through each aug folder and move to data/test
-print('migrating test...')
-file_names = os.listdir('test/')
-for file_name in file_names:
-    shutil.move(os.path.join(source_dir, file_name), test_dir)
-print('done migrating',source_dir)
-
-
-
 #cleaning .xml.txt
 print('cleaning text')
 for path in glob.glob(train_dir+'/*.xml.txt'):
@@ -47,3 +30,11 @@ for path in glob.glob(train_dir+'/*.xml.txt'):
     os.rename(path,new_path)
 print('done')
 print()
+
+
+#moving all config shit
+home = os.getcwd() 
+new_home = home.replace('eolas','darknet')
+shutil.move(home+'/obj.names',new_home+'/data/obj.names')
+shutil.move(home+'/obj.data',new_home+'/data/obj.data')
+shutil.move(home+'/CUSTOM.cfg',new_home+'/cfg/CUSTOM.cfg')
--- gen_text.py
+++ gen_text.py
@@ -1,22 +1,25 @@
 import glob,os
 
 home = os.getcwd()
+new_home = home.replace('eolas','darknet')
+os.chdir(new_home)
+print(new_home)
 
-#generate text file for training images
-image_paths = glob.glob('darknet/data/obj/*.png')
+# #generate text file for training images
+image_paths = glob.glob(new_home+'/data/obj/*.png')
 #image_paths = [home+'/'+im for im in image_files]
 
-with open("darknet/data/train.txt", "w") as outfile:
+with open(new_home+"/data/train.txt", "w") as outfile:
     for image in image_paths:
         outfile.write(image)
         outfile.write("\n")
     outfile.close()
 
 #generate text file for test images
-image_paths = glob.glob('darknet/data/test/*.png')
+image_paths = glob.glob(new_home+'/data/test/*.png')
 #image_paths = [home+'/'+im for im in image_files]
 
-with open("darknet/data/test.txt", "w") as outfile:
+with open(new_home+"/data/test.txt", "w") as outfile:
     for image in image_paths:
         outfile.write(image)
         outfile.write("\n")
